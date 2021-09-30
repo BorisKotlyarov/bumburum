7a48bf9388f93c920a790ce289d3e6b10f3670ab
Yevhenii Nykytenko <dzekinnik@gmail.com>
2021-09-30 19:47:42 +0300

    typescript typings

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+export type Color = 'Black' | 'Red' | 'Green' | 'Yellow' | 'Blue' | 'Magenta' | 'Cyan' | 'White';
+
+export type ColorMap = Record<Color, string>
+
+export const font: ColorMap;
+
+export const background: ColorMap;
